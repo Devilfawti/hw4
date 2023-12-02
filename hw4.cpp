46af1c5b451fa4cdbce1884a838cd43a89d9f9f7
Devilfawti <152597635+Devilfawti@users.noreply.github.com>
2023-12-02 22:45:33 +0330

    Add files via upload

---
+++ hw4.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std ;
+main () {
+	
+	
+	float a,b,c,d,e,f;
+	
+cout<<"please enter a number for a:";
+	
+cin>>a;
+	
+cout<<"please enter a number for b :";
+
+cin>>b;
+c=(a+b);
+d=(a-b);
+e=(a*b);
+f=(int)a%(int)b;
+	
+cout<<"c:"<<c<<"\n"<<"d:"<<d<<"\n"<<"e:"<<e<<"\n"<<f;
+}
